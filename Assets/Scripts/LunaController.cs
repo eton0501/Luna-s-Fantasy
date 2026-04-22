76b84f90fc28e61730b4084451dd0100eb166839
eton0501 <etonn103@gmail.com>
2026-04-22 17:42:03 +0800

    feat:動畫組件、控制腳本、動畫狀態

--- Assets/Scripts/LunaController.cs
+++ Assets/Scripts/LunaController.cs
@@ -8,20 +8,52 @@ public class LunaController : MonoBehaviour
     public int MaxHealth{get{return maxHealth;}}
     private int currentHealth;
     public int Health{get{return currentHealth;}}
+    private Animator animator;
+    private Vector2 lookDirection=new Vector2(1,0);
+    private float moveScale;
+    private Vector2 move;
     void Start()
     {
         rigidbody2D=GetComponent<Rigidbody2D>();
         maxHealth=5;
         currentHealth=maxHealth;
+        animator=GetComponentInChildren<Animator>();
     }
+
 
     void Update()
     {
-        float horizontal=Input.GetAxis("Horizontal");
-        float vertical=Input.GetAxis("Vertical");
+        float horizontal=Input.GetAxisRaw("Horizontal");
+        float vertical=Input.GetAxisRaw("Vertical");
+        move=new Vector2(horizontal,vertical);
+        if (!Mathf.Approximately(move.x, 0) || !Mathf.Approximately(move.y, 0))
+        {
+            lookDirection.Set(move.x,move.y);
+            lookDirection.Normalize();
+        }
+        animator.SetFloat("LookX",lookDirection.x);
+        animator.SetFloat("LookY",lookDirection.y);
+        moveScale=move.magnitude;
+        if (move.magnitude > 0)
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                moveScale=1;
+                moveSpeed=2;
+            }
+            else
+            {
+                moveScale=2;
+                moveSpeed=3.5f;
+            }
+        }
+        animator.SetFloat("MoveValue",moveScale);
+
+    }
+    void FixedUpdate()
+    {
         Vector2 position=transform.position;
-        position.x=position.x+moveSpeed*horizontal*Time.deltaTime;
-        position.y=position.y+moveSpeed*vertical*Time.deltaTime;
+        position=position+moveSpeed*move*Time.fixedDeltaTime;
         rigidbody2D.MovePosition(position);
     }
     public void ChangeHealth(int amount)
